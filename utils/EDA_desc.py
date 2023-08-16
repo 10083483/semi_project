8600f8babc7b12a8426fedc33cecfd09afd97e38
hansol <58614489+10083483@users.noreply.github.com>
2023-08-16 19:13:04 +0900

    Update EDA_desc.py

--- utils/EDA_desc.py
+++ utils/EDA_desc.py
@@ -9,15 +9,12 @@ def desc():
     
     BeforeCOVID19_1 =pd.read_csv('data/1.boxofficeD.csv',encoding='utf-8-sig')
     AfterCOVID19_1 =pd.read_csv('data/1.boxofficeA.csv',encoding='utf-8-sig')
-    st.dataframe(BeforeCOVID19_1)
-    st.dataframe(AfterCOVID19_1)
+    #st.dataframe(BeforeCOVID19_1)
     BeforeCOVID19_1 = BeforeCOVID19_1.astype({'salesAmt':'float','audiCnt':'float'})
     AfterCOVID19_1 = AfterCOVID19_1.astype({'salesAmt':'float','audiCnt':'float'})
-    #grafe
     categories = ['During Covid19 Restriction', 'After Covid19 Restriction']
     values = [BeforeCOVID19_1.sum()['salesAmt'],AfterCOVID19_1.sum()['salesAmt']]
     
-    st.write(values)
     fig, ax = plt.subplots()
     ax.bar(categories, values)
     
